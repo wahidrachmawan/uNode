4bcf52e6ab87dc35511209887fd0ceeeeedc9301
wahidrachmawan <wahidrachmawan@yahoo.co.id>
2024-10-01 16:33:42 +0700

    improved editor

--- Assets/uNode3/Editor/CustomEditor/ScriptGraphEditor.cs
+++ Assets/uNode3/Editor/CustomEditor/ScriptGraphEditor.cs
@@ -6,20 +6,25 @@ using System.Collections.Generic;
 
 namespace MaxyGames.UNode.Editors {
     [CustomEditor(typeof(ScriptGraph), true)]
-    public class ScriptGraphEditor : Editor {
-		public override void OnInspectorGUI() {
-			var monoScript = uNodeEditorUtility.GetMonoScript(target);
-			if(monoScript != null) {
-				EditorGUI.BeginDisabledGroup(true);
-				EditorGUILayout.ObjectField("Script", monoScript, typeof(MonoScript), true);
-				EditorGUI.EndDisabledGroup();
-			}
+    public class ScriptGraphEditor : GraphAssetEditor {
+		public override void DrawGUI(bool isInspector) {
 			var asset = target as ScriptGraph;
 			asset.Namespace = EditorGUILayout.DelayedTextField("Namespace", asset.Namespace);
 			uNodeGUI.DrawNamespace("Using Namespaces", asset.UsingNamespaces, asset, (arr) => {
 				asset.UsingNamespaces = arr as List<string> ?? arr.ToList();
 				uNodeEditorUtility.MarkDirty(asset);
 			});
+			if(isInspector) {
+				EditorGUILayout.LabelField("Classes", EditorStyles.centeredGreyMiniLabel);
+				using(new EditorGUILayout.VerticalScope("Box")) {
+					foreach(IScriptGraphType type in asset.TypeList) {
+						if(type == null) continue;
+						if(GUILayout.Button(type.TypeName)) {
+							uNodeEditor.Open(type);
+						}
+					}
+				}
+			}
 		}
 	}
 }
